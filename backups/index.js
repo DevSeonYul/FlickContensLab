bda1222d244bc51304dbb39e7113678ec159772e
DevSeonYul <57772111+DevSeonYul@users.noreply.github.com>
2019-11-15 04:46:22 +0900

    upload origin website
    domain 만료

---
+++ backups/index.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    focusRollPlay();
+
+    // var
+    var focusCnt = $('.content .mainBanner>li').length;
+    var focusIndex = 1;
+
+    // func
+    function focusNext() {
+        $('.content .mainBanner>li:first').appendTo('.content .mainBanner');
+        onlyFirstFocus();
+        forcusIndex++;
+        if (focusIndex > focusCount) { focusIndex = 1; }
+    }
+    function focusRollPlay() {
+        focus_roll = setInterval(function () { focusNext(); }, 5000);
+    }
+    function focusRollStop() {
+        clearTimeout(focus_roll);
+    }
+    function onlyFirstFocus() {
+        $('.content .mainBanner>li').hide().eq(0).show();
+    }
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+function notready() {
+    alert("준비 중입니다.");
+}
